878deef3d16751a9d18915230010752943719fa6
rizfan radya <140232358+rizfanradya@users.noreply.github.com>
2024-03-05 00:31:17 +0700

    chart

--- src/app/(pages)/dashboard/page.tsx
+++ src/app/(pages)/dashboard/page.tsx
@@ -1,6 +1,6 @@
 import prisma from "@/utils/db";
-import ButtonSubmit from "./buttonSubmit";
-import { redirect } from "next/navigation";
+import AddDeposito from "./addDeposito";
+import DashboardLineChartData from "./chartData";
 
 export default async function Dashboard() {
   const deposit = await prisma.deposit.findMany();
@@ -9,78 +9,10 @@ export default async function Dashboard() {
     0
   );
 
-  async function submitDeposit(formData: FormData) {
-    "use server";
-    const amountStr: any = formData.get("amount");
-    const deposit: any = formData.get("deposit");
-    const amount: any = parseInt(amountStr);
-
-    async function addData() {
-      try {
-        const deposito = await prisma.deposit.create({
-          data: { deposit, amount },
-        });
-        return { message: "ok", deposito };
-      } catch (error) {
-        return { message: "error", error };
-      }
-    }
-
-    const response = await addData();
-    if (response.message === "ok") {
-      redirect("/addDepositoSuccess");
-    } else {
-      redirect("/errorAddDeposito");
-    }
-  }
-
   return (
-    <div>
-      <div className="flex items-center justify-center gap-2">
-        <div className="flex items-center gap-4 bg-slate-950 text-white w-max p-3 rounded-xl px-4">
-          <span className="rounded-full bg-red-500 text-white p-1 text-xs">
-            Rp
-          </span>
-          <span className="text-xl">
-            {totalAmount.toLocaleString("id-ID")},-
-          </span>
-        </div>
-
-        <label
-          htmlFor="my_modal_7"
-          className="btn btn-success text-4xl text-white"
-        >
-          +
-        </label>
-        <input type="checkbox" id="my_modal_7" className="modal-toggle" />
-        <div className="modal" role="dialog">
-          <div className="modal-box">
-            <h3 className="text-lg font-bold">Add New Deposit</h3>
-            <form className="mt-6 grid gap-4" action={submitDeposit}>
-              <input
-                className="input input-bordered"
-                placeholder="Deposit"
-                name="deposit"
-                required
-              />
-              <input
-                className="input input-bordered"
-                placeholder="Total"
-                name="amount"
-                type="number"
-                required
-              />
-              <div className="modal-action">
-                <ButtonSubmit />
-              </div>
-            </form>
-          </div>
-          <label
-            className="modal-backdrop cursor-pointer"
-            htmlFor="my_modal_7"
-          ></label>
-        </div>
-      </div>
+    <div className="p-4">
+      <AddDeposito totalAmount={totalAmount} />
+      <DashboardLineChartData />
     </div>
   );
 }
