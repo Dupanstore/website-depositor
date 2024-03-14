adbe82818b9cd4dcc0e38071ad9c662dbb8eada0
rizfan radya <140232358+rizfanradya@users.noreply.github.com>
2024-03-15 00:20:07 +0700

    up

---
+++ prisma/migrations/20240314170829_dev/migration.sql
@@ -0,0 +1,98 @@
+-- CreateTable
+CREATE TABLE `user` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `username` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `role` VARCHAR(191) NOT NULL,
+    `maxWin` INTEGER NOT NULL,
+
+    UNIQUE INDEX `user_username_key`(`username`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `deposit` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `nominal_deposit` INTEGER NOT NULL,
+    `proof_transaction` VARCHAR(191) NOT NULL,
+    `status` VARCHAR(191) NOT NULL,
+    `sender_name` VARCHAR(191) NOT NULL,
+    `sender_rekening` VARCHAR(191) NOT NULL,
+    `sender_bank` VARCHAR(191) NOT NULL,
+    `recipient_name` VARCHAR(191) NOT NULL,
+    `recipient_rekening` VARCHAR(191) NOT NULL,
+    `recipient_bank` VARCHAR(191) NOT NULL,
+    `user_id` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `rekening` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `no_rekening` VARCHAR(191) NOT NULL,
+    `bank` VARCHAR(191) NOT NULL,
+    `user_id` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `bank` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `bank` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `bank_bank_key`(`bank`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `betting` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `nominal` INTEGER NOT NULL,
+    `time` INTEGER NOT NULL,
+    `status` VARCHAR(191) NOT NULL,
+    `speed` INTEGER NOT NULL,
+    `user_id` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `withdraw` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `no_rekening` VARCHAR(191) NOT NULL,
+    `bank` VARCHAR(191) NOT NULL,
+    `nominal` INTEGER NOT NULL,
+    `status` VARCHAR(191) NOT NULL,
+    `user_id` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `deposit` ADD CONSTRAINT `deposit_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `rekening` ADD CONSTRAINT `rekening_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `betting` ADD CONSTRAINT `betting_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `withdraw` ADD CONSTRAINT `withdraw_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
--- src/app/api/getServerTime/route.ts
+++ src/app/api/getServerTime/route.ts
@@ -1,6 +1,8 @@
+import { unstable_noStore } from "next/cache";
 import { NextResponse } from "next/server";
 
 export function GET() {
+  unstable_noStore();
   const serverTime = Date.now();
   return NextResponse.json({ time: serverTime });
 }
