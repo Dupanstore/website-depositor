9fed382d0aee75915af684ab5344f9bf8f70c8fb
rizfan radya <140232358+rizfanradya@users.noreply.github.com>
2024-03-18 05:07:42 +0700

    experimental

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: { serverActions: { allowedOrigins: ["*"] } },
+};
 
 export default nextConfig;
